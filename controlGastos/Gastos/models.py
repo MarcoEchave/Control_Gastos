73097311a5bad3d8a421e99c6e6b56d6cb675d33
MarcoEchavee <marcoechave@gmail.comm>
2023-10-01 18:56:45 -0700

    Create model Banco and Categoria

--- controlGastos/Gastos/models.py
+++ controlGastos/Gastos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class Banco(models.Model):
+    banco = models.CharField("Banco",max_length=30)
+
+    def __str__(self):
+        return self.banco
+    
+class Categoria(models.Model):
+    categoria = models.CharField("Categoria",max_length=30)
+
+    def __str__(self):
+        return self.categoria
