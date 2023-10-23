993262e2ee9bed3b2d9f9355471572eb0e88e342
MarcoEchave <marcoechave@gmail.com>
2023-10-22 21:16:08 -0700

    Create models users

--- controlGastos/user/models.py
+++ controlGastos/user/models.py
@@ -1,6 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import UserManager
 
 # Create your models here.
-class Users(models.Model):
+class Users(AbstractBaseUser,PermissionsMixin):
+    username = models.CharField(max_length=10, unique=True)
     nombre = models.CharField("Nombre",max_length=50)
     password = models.CharField("Password",max_length=30)
+    email = models.EmailField(default="a@dominio.com")
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email',]
+
+    objects = UserManager()
+
+    def get_short_name(self):
+        return self.username
+    
--- controlGastos/user/urls.py
+++ controlGastos/user/urls.py
@@ -1,8 +1,12 @@
-from django.contrib import admin
 from django.urls import path
-
 from .views import index
+from . import views
 
 urlpatterns = [
-    path('', index),
+    path('', index,name="index"),
+    path('register', views.UserRegisterView.as_view(),name='user-register'),
+    path('login', views.LoginUser.as_view(),name='user-login'),
+    path('logout',views.LogoutView.as_view(),name='user-logout'),
+    path('update',views.UpdatePasswordView.as_view(),name='user-update'),
+
 ]
